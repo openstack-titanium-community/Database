e1fe88292f60f4ff2454cc892817ef74413a2f09
Keith Rochford <keith.rochford@gmail.com>
2013-10-11 12:19:14 +0100

    add client and monitoring recipes to the runlist

--- barclamp/percona/chef/roles/percona.rb
+++ barclamp/percona/chef/roles/percona.rb
@@ -1,7 +1,9 @@
 name "percona"
 description "Percona Role"
 run_list(
-        "recipe[percona::cluster]"
+        "recipe[percona::client]",
+        "recipe[percona::cluster]",
+        "recipe[percona::monitoring]"
 )
 default_attributes()
 override_attributes()
